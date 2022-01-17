52cec30023ba395b1eec641154cc4ef0652e591e
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-01-17 16:18:34 +0530

    hompage

--- unit-4/sprint-4/evaluation/scripts/main.js
+++ unit-4/sprint-4/evaluation/scripts/main.js
@@ -1,6 +1,15 @@
 async function apiCall(url) {
 
+   try{
+       let res =await fetch(url)
 
+       let data =await res.json()
+
+       return data
+   }
+   catch(err){
+     console.log(error)
+   }
     //add api call logic here
 
 
@@ -10,6 +19,25 @@ async function apiCall(url) {
 function appendArticles(articles, main) {
 
     //add append logic here
+    articles.forEach(({title,description,image })=>{
+          let div =document.createElement("div")
+          
+          let div2 =document.createElement("div")
+          div2.onclick
+
+          let titleof = document.createElement("h2")
+          titleof.textContent =title
+          
+          let desc = document.createElement("p")
+          desc.textContent =description
+
+          let imag = document.createElement("img")
+          imag.src =image
+           div2.append(desc,imag)
+           div2.style.display ="flex"
+          div.append(titleof,div2)
+          main.append(div)
+    })
 
 }
 
--- unit-4/sprint-4/evaluation/scripts/search.js
+++ unit-4/sprint-4/evaluation/scripts/search.js
@@ -1,6 +1,6 @@
 function storeSearchterm(term) {
-
-
+   
+   
 }
 
 export default storeSearchterm
