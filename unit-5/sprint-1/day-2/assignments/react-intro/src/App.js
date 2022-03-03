df34a6774ebffdc222fdabeab329ad0c3679c649
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-03-03 23:35:59 +0530

    done

--- unit-5/sprint-1/day-2/assignments/react-intro/src/App.js
+++ unit-5/sprint-1/day-2/assignments/react-intro/src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
-    const os =["Android","Blackberry","iPhone","Windows Phone"]
-    const company =["Samsung","HTC","Micromax","Apple"]
+  const os = ["Android", "Blackberry", "iPhone", "Windows Phone"];
+  const company = ["Samsung", "HTC", "Micromax", "Apple"];
   return (
-    <div className="App">
-      
+    <div>
+      <div>
+        <h2>Mobile Operating System</h2>
+        <div>{os.map((el) => com1(el))}</div>
+      </div>
+      <div>
+        <h2>Mobile Manufactures</h2>
+        <div>{company.map((el)=> com2(el))}</div>
+      </div>
     </div>
   );
 }
-function com1(){
-
+function com1(name) {
+  return (
+    // <h2>f</h2>
+    <div>
+      <ul>
+        <li>{name}</li>
+      </ul>
+    </div>
+  );
+}
+function com2(e){
+  return(
+    <div>
+     
+        <ul id="list">
+          <li>{e}</li>
+        </ul>
+     
+    </div>
+  )
 }
 export default App;
