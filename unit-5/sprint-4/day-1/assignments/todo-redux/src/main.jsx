586ca20fff55a37bfef947439145d4adb7c08ef5
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-03-22 20:35:09 +0530

    one

---
+++ unit-5/sprint-4/day-1/assignments/todo-redux/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider >
+
+    <BrowserRouter >
+    
+    <App />
+    </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
